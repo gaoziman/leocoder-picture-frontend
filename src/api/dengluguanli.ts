7c037507d7775be67052390409ed9d4ad47d4651
leocoder <2942894660@qq.com>
2024-12-11 21:53:24 +0800

    feat: 用户管理开发

---
+++ src/api/dengluguanli.ts
@@ -0,0 +1,49 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/utils/request'
+
+/** 获取登录用户信息 GET /api/user/get/login */
+export async function getLoginUserUsingGet(options?: { [key: string]: any }) {
+  return request<API.ResultLoginUserVO_>('/api/user/get/login', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** 用户登录 POST /api/user/login */
+export async function userLoginUsingPost(
+  body: API.UserLoginRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultLoginUserVO_>('/api/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 用户注销 POST /api/user/logout */
+export async function userLogoutUsingPost(options?: { [key: string]: any }) {
+  return request<API.ResultBoolean_>('/api/user/logout', {
+    method: 'POST',
+    ...(options || {}),
+  })
+}
+
+/** 用户注册 POST /api/user/register */
+export async function userRegisterUsingPost(
+  body: API.UserRegisterRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultLong_>('/api/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
---
+++ src/api/yonghuguanli.ts
@@ -0,0 +1,90 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/utils/request'
+
+/** 创建用户 POST /api/user/add */
+export async function addUserUsingPost(body: API.UserAddRequest, options?: { [key: string]: any }) {
+  return request<API.ResultLong_>('/api/user/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 删除用户 POST /api/user/delete */
+export async function deleteUserUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultBoolean_>('/api/user/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 获取用户（仅管理员） GET /api/user/get */
+export async function getUserByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getUserByIdUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultUser_>('/api/user/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 根据 id 获取包装类 GET /api/user/get/vo */
+export async function getUserVoByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getUserVOByIdUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultUserVO_>('/api/user/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 分页获取用户封装列表（仅管理员） POST /api/user/list/page/vo */
+export async function listUserVoByPageUsingPost(
+  body: API.UserQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultPageUserVO_>('/api/user/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 更新用户 POST /api/user/update */
+export async function updateUserUsingPost(
+  body: API.UserUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.ResultBoolean_>('/api/user/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
